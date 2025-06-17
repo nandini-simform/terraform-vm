cf6ea15bcec4b78f32ff2629f341bff87870e0f4
Nandini Vyas <nandini.vyas@simformsolutions.com>
2025-06-17 11:05:19 +0530

    added some changes

--- main.tf
+++ main.tf
@@ -4,13 +4,14 @@ module "vm_module" {
   resource_group_location = var.resource_group_location
   vm_name                 = var.vm_name
   vm_size                 = var.vm_size
-  virtual_network_name    = ["10.0.0.0/22"]
-  subnet_address_space    = ["10.0.1.0/24"]
-  vnet_address_space      = var.vnet_address_space
-  public_ip_name          = var.public_ip_name
-  nic                     = var.nic
-  enable_bastion          = false
-  enable_nsg = false
-  nsg_rules = false
-  
+  admin_username          = var.admin_username
+  authentication_type     = "ssh"
+  generate_ssh_key        = true
+
+  image_publisher         = "Canonical"
+  image_offer             = "UbuntuServer"
+  image_sku               = "18.04-LTS"
+  image_version           = "latest"
+  nic = var.nic
+
 }
---
+++ variables.tf
@@ -0,0 +1,89 @@
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "The location of the resource group"
+  type        = string
+}
+
+
+variable "nic" {
+  description = "The name of the network interface"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "The name of the virtual machine"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "The size of the virtual machine"
+  type        = string
+}
+variable "enable_bastion" {
+  description = "Whether to create Azure Bastion host"
+  type        = bool
+  default     = false
+}
+
+variable "image_publisher" {
+  description = "VM image publisher"
+  type        = string
+  default     = "Canonical"
+}
+
+variable "image_offer" {
+  description = "VM image offer"
+  type        = string
+  default     = "0001-com-ubuntu-server-jammy"
+}
+
+variable "image_sku" {
+  description = "VM image SKU"
+  type        = string
+  default     = "22_04-lts"
+}
+
+variable "image_version" {
+  description = "VM image version"
+  type        = string
+  default     = "latest"
+}
+
+variable "generate_ssh_key" {
+  description = "Whether to generate a new SSH key or use a provided one"
+  type        = bool
+  default     = true
+}
+
+variable "public_ssh_key" {
+  description = "The public SSH key to use when generate_ssh_key is false"
+  type        = string
+  default     = ""
+}
+variable "authentication_type" {
+  description = "Authentication method: ssh or password"
+  type        = string
+  default     = "ssh"
+  validation {
+    condition     = contains(["ssh", "password"], var.authentication_type)
+    error_message = "authentication_type must be either 'ssh' or 'password'."
+  }
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "admin_password" {
+  description = "Admin password for the VM (required if authentication_type is 'password')"
+  type        = string
+  default     = ""
+  sensitive   = true
+}
